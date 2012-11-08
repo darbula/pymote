19af75134b19ed008f261642d26060b47eaafab3
Damir Arbula <damir.arbula@gmail.com>
2012-11-08 03:12:20 +0100

    docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -244,3 +244,6 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'http://docs.python.org/': None}
+
+# for default blue theme on readthedocs 
+html_style = None
