7c26e612144f9242655a8d19d49dffb96d88f822
Damir Arbula <damir.arbula@gmail.com>
2013-06-11 17:23:01 +0200

    readsensors params standardization

--- pymote/algorithms/readsensors.py
+++ pymote/algorithms/readsensors.py
@@ -2,12 +2,11 @@ from pymote.algorithm import NetworkAlgorithm
 
 
 class ReadSensors(NetworkAlgorithm):
-
     """Read all sensors and save data in memory for latter usage."""
+    required_params = ()
+    default_params = {'sensorReadingsKey': 'sensorReadings'}
     
     def run(self):
-        if not hasattr(self, 'sensorReadingsKey'):
-            self.sensorReadingsKey = 'sensorReadings'
         for node in self.network.nodes():
             node.memory.update({self.sensorReadingsKey:
                                 node.compositeSensor.read()})
