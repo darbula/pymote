101aeedbfcc1fcec524bea4f28587afeeeb07bef
Damir Arbula <damir.arbula@gmail.com>
2013-12-04 19:01:36 +0100

    algorithm metaclass handles initialization of required and default algorithm class params

--- pymote/algorithm.py
+++ pymote/algorithm.py
@@ -1,16 +1,81 @@
 from pymote.message import Message
 from pymote.logger import logger
+from inspect import getmembers
+
+
+class AlgorithmMeta(type):
+    """ Metaclass for required and default params extension and update. """
+
+    def __new__(cls, clsname, bases, dct):
+        """
+        Collect required and default params from class bases and extend and
+        update those in dct that have been sent for new class.
+
+        """
+        rps = []
+        dps = {}
+        for base in bases[::-1]:
+            base_rps = dict(getmembers(base)).get('required_params', [])
+            rps.extend(base_rps)
+            base_dps = dict(getmembers(base)).get('default_params', {})
+            dps.update(base_dps)
+        rps.extend(dct.get('required_params', []))
+        dps.update(dct.get('default_params', {}))
+        all_params = rps + dps.keys()
+
+        assert len(rps)==len(set(rps)), \
+            'Some required params %s defined in multiple classes.' % str(rps)
+        assert len(all_params)==len(set(all_params)), \
+            'Required params %s and default params %s should be unique.' % \
+            (str(rps), str(dps.keys()))
+
+        dct['required_params'] = tuple(rps)
+        dct['default_params'] = dps
+        return super(AlgorithmMeta, cls).__new__(cls, clsname, bases, dct)
 
 
 class Algorithm(object):
-
     """
     Abstract base class for all algorithms.
 
-    Currently there are two subclasses NodeAlgorithm for distributed algorithms
-    and NetworkAlgorithm for centralized algorithms.
+    Currently there are two main subclasses:
+        * NodeAlgorithm used for distributed algorithms
+        * NetworkAlgorithm used for centralized algorithms
+
+    When writing new algorithms make them subclass either of NodeAlgorithm or
+    NetworkAlgorithm.
+
+    Every algorithm instance has a set of required and default params:
+        * Required params must be given to algorithm initializer as a keyword
+            arguments.
+        * Default params can be given to algorithm initializer as a keyword
+            arguments, if not their class defines default value.
+
+    Note: On algorithm initialization all params are converted to instance
+    attributes.
+
+    For example:
+
+    class SomeAlgorithm(NodeAlgorithm):
+        required_params = ('rp1',)
+        default_params = {'dp1': 'dv1',}
+
+    >>> net = Network()
+    >>> alg = SomeAlgorithm(net, rp1='rv1')
+    >>> alg.rp1
+    'rv1'
+    >>> alg.dp1
+    'dv1'
+
+    Params in algorithm subclasses are inherited from its base classes, that
+    is, required params are extended and default are updated:
+        * required_params are union of all required params of their ancestor
+            classes.
+        * default_params are updated so default values are overridden in
+            subclasses
 
     """
+    __metaclass__ = AlgorithmMeta
 
     required_params = ()
     default_params = {}
@@ -18,13 +83,7 @@ class Algorithm(object):
     def __init__(self, network, **kwargs):
         self.network = network
         self.name = self.__class__.__name__
-        logger.debug('Instance of %s class has been initialized.' %
-                    self.name)
-
-        for default_param in self.default_params.keys():
-            if default_param in self.required_params:
-                raise PymoteAlgorithmException('Default param cannot be'
-                                               'required param.')
+        logger.debug('Instance of %s class has been initialized.' % self.name)
 
         for required_param in self.required_params:
             if required_param not in kwargs.keys():
--- pymote/tests/test_algorithm.py
+++ pymote/tests/test_algorithm.py
@@ -25,9 +25,29 @@ class SomeNetworkAlgorithm(NetworkAlgorithm):
                       'dp3': 'dp3_value'}
 
 
-class SomeAlgorithmWhereDpIsRp(NetworkAlgorithm):
-    required_params = ('rdp1',)
-    default_params = {'rdp1': 'dp1_value', }
+class SomeAlgorithmWithInheritance(SomeNodeAlgorithm):
+    required_params = ('rp4',)
+    default_params = {'dp4': 'dp4_value', }
+
+
+class SomeAlgorithmWithInheritanceChild(SomeAlgorithmWithInheritance):
+    required_params = ('rp5', 'rp6')
+    default_params = {'dp2': 'overriden_dp2_value', }
+
+
+def rp_multiple():
+    class SomeAlgorithmWhereRpIsRedefined(SomeAlgorithmWithInheritance):
+        required_params = ('rp2',)
+
+
+def dp_is_rp():
+    class SomeAlgorithmWhereDpIsInheritedRp(SomeAlgorithmWithInheritance):
+        default_params = {'rp2': 'dp2_value', }
+
+
+def rp_is_dp():
+    class SomeAlgorithmWhereRpIsInheritedDp(SomeAlgorithmWithInheritance):
+        required_params = ('dp2',)
 
 
 class TestAlgorithmsSetter(unittest.TestCase):
@@ -42,6 +62,13 @@ class TestAlgorithmsSetter(unittest.TestCase):
                               {}),
 
                               SomeNetworkAlgorithm,
+
+                              (SomeAlgorithmWithInheritance,
+                               {'rp1': 1, 'rp2': 2, 'rp3': 3, 'rp4': 4}),
+
+                              (SomeAlgorithmWithInheritanceChild,
+                               {'rp1': 1, 'rp2': 2, 'rp3': 3, 'rp4': 4,
+                                'rp5': 5, 'rp6': 6}),
                               )
         self.check = [
                 # wrong_format
@@ -52,9 +79,8 @@ class TestAlgorithmsSetter(unittest.TestCase):
                 # missing_req_params
                 (PymoteAlgorithmException, ((SomeNodeAlgorithm,
                                              {'rp1': 1, }),)),
-                # dp_is_rp
-                (PymoteAlgorithmException, ((SomeAlgorithmWhereDpIsRp,
-                                             {'rdp1': 1, }),)),
+                (PymoteAlgorithmException, ((SomeAlgorithmWithInheritance,
+                                             {'rp1': 1, }),)),
                 ]
 
     def test_setter(self):
@@ -63,10 +89,24 @@ class TestAlgorithmsSetter(unittest.TestCase):
         for exc, alg in self.check:
             self.assertRaises(exc, set_algorithms, self.net, alg)
 
-    def test_default_params(self):
-        """Test default params."""
-        self.net.algorithms = ((SomeNetworkAlgorithm,
-                                {'dp2': 'overriden_dp2_value', }),)
+    def test_params_inheritance(self):
+        """Test default params inheritance algorithm classes."""
+        self.net.algorithms = ((SomeAlgorithmWithInheritanceChild,
+                                {'rp1': 1, 'rp2': 2, 'rp3': 3, 'rp4': 4,
+                                'rp5': 5, 'rp6': 6}),)
         self.assertTrue(self.net.algorithms[0].dp1 == 'dp1_value')
         self.assertTrue(self.net.algorithms[0].dp2 == 'overriden_dp2_value')
         self.assertTrue(self.net.algorithms[0].dp3 == 'dp3_value')
+        self.assertRaises(AssertionError, rp_multiple)
+        self.assertRaises(AssertionError, dp_is_rp)
+        self.assertRaises(AssertionError, rp_is_dp)
+
+    def test_default_params(self):
+        """Test default params."""
+        self.net.algorithms = ((SomeNetworkAlgorithm,
+                                {'dp2': 'overriden_dp2_value', }),
+                               )
+        self.assertTrue(self.net.algorithms[0].dp1 == 'dp1_value')
+        self.assertTrue(self.net.algorithms[0].dp2 == 'overriden_dp2_value')
+        self.assertTrue(self.net.algorithms[0].dp3 == 'dp3_value')
+
