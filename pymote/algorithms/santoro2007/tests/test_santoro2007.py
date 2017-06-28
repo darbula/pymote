9befd6a5598c4de870ff1d2513099e23398bb39b
Matko <matkoburul@gmail.com>
2017-06-28 19:37:37 +0200

    Remove do_test method from test_santoro2007

--- pymote/algorithms/santoro2007/tests/test_santoro2007.py
+++ pymote/algorithms/santoro2007/tests/test_santoro2007.py
@@ -6,24 +6,6 @@ from pymote.algorithms.santoro2007.yoyo import YoYo
 
 class TestSantoro2007(unittest.TestCase):
 
-    def do_test(self, net, name):
-        net.algorithms = (YoYo, )
-        sim = Simulation(net, logLevel='WARNING')
-        sim.run()
-
-        min_id = min(sim.network.nodes(), key=lambda node: node.id).id
-        for node in sim.network.nodes():
-            if node.id == min_id:
-                # Check if the node with the smallest ID is the LEADER
-                assert node.status == 'LEADER', \
-                    '%s: Node %d has status %s, not LEADER' % \
-                    (name, node.id, node.status)
-            else:
-                # Check if every other node is PRUNED
-                assert node.status == 'PRUNED', \
-                    '%s: Node %d has status %s, not PRUNED' % \
-                    (name, node.id, node.status)
-
     def test_santoro2007(self):
         N_ITERS = 5
         N_NETWORKS = 15
@@ -40,10 +22,27 @@ class TestSantoro2007(unittest.TestCase):
 
         for i, node_positions in enumerate(nets, start=1):
             net = Network()
-            for node_pos, name in node_positions:
+            for node_pos in node_positions:
                 net.add_node(pos=node_pos, commRange=node_range)
 
-            self.do_test(net, 'Special %d' % i)
+            name = 'Special %d' % i
+
+            net.algorithms = (YoYo, )
+            sim = Simulation(net, logLevel='WARNING')
+            sim.run()
+
+            min_id = min(sim.network.nodes(), key=lambda node: node.id).id
+            for node in sim.network.nodes():
+                if node.id == min_id:
+                    # Check if the node with the smallest ID is the LEADER
+                    assert node.status == 'LEADER', \
+                        '%s: Node %d has status %s, not LEADER' % \
+                        (name, node.id, node.status)
+                else:
+                    # Check if every other node is PRUNED
+                    assert node.status == 'PRUNED', \
+                        '%s: Node %d has status %s, not PRUNED' % \
+                        (name, node.id, node.status)
 
         for i in range(N_ITERS):
             for n_nodes in range(N_NODES_STEP,
@@ -52,4 +51,22 @@ class TestSantoro2007(unittest.TestCase):
 
                 net_gen = NetworkGenerator(n_nodes)
                 net = net_gen.generate_random_network()
-                self.do_test(net, 'Random %d, %d nodes' % (i, n_nodes))
+
+                name = 'Random %d, %d nodes' % (i, n_nodes)
+
+                net.algorithms = (YoYo, )
+                sim = Simulation(net, logLevel='WARNING')
+                sim.run()
+
+                min_id = min(sim.network.nodes(), key=lambda node: node.id).id
+                for node in sim.network.nodes():
+                    if node.id == min_id:
+                        # Check if the node with the smallest ID is the LEADER
+                        assert node.status == 'LEADER', \
+                            '%s: Node %d has status %s, not LEADER' % \
+                            (name, node.id, node.status)
+                    else:
+                        # Check if every other node is PRUNED
+                        assert node.status == 'PRUNED', \
+                            '%s: Node %d has status %s, not PRUNED' % \
+                            (name, node.id, node.status)
