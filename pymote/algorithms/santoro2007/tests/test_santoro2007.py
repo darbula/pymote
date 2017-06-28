af79f07b928fa2525de785b736a970553573253a
Matko <matkoburul@gmail.com>
2017-06-28 19:18:46 +0200

    Fixed test_santoro2007

--- pymote/algorithms/santoro2007/tests/test_santoro2007.py
+++ pymote/algorithms/santoro2007/tests/test_santoro2007.py
@@ -2,70 +2,52 @@ from pymote import NetworkGenerator, Simulation, write_pickle, Network
 from pymote.algorithms.santoro2007.yoyo import YoYo
 
 
-def do_test(net):
-    net.algorithms = (YoYo, )
-    sim = Simulation(net, logLevel='WARNING')
-    try:
-        sim.run()
-    except e:
-        net.show()
-        write_pickle(net, 'TestYoYo.npc.gz')
-        print(e)
-        raw_input('Showing graph plot, press ENTER to continue')
-        exit()
+class TestSantoro2007(unittest.TestCase):
 
-    min_id = min(sim.network.nodes(),
-                 key=lambda node: node.id).id
-    for node in sim.network.nodes():
-        try:
+    def do_test(self, net, name):
+        net.algorithms = (YoYo, )
+        sim = Simulation(net, logLevel='WARNING')
+        sim.run()
+
+        min_id = min(sim.network.nodes(), key=lambda node: node.id).id
+        for node in sim.network.nodes():
             if node.id == min_id:
                 # Check if the node with the smallest ID is the LEADER
                 assert node.status == 'LEADER', \
-                    'Node %d has status %s, not LEADER' % \
-                    (node.id, node.status)
+                    '%s: Node %d has status %s, not LEADER' % \
+                    (name, node.id, node.status)
             else:
                 # Check if every other node is PRUNED
                 assert node.status == 'PRUNED', \
-                    'Node %d has status %s, not PRUNED' % \
-                    (node.id, node.status)
+                    '%s: Node %d has status %s, not PRUNED' % \
+                    (name, node.id, node.status)
 
-        except AssertionError as e:
-            net.show()
-            write_pickle(net, 'TestYoYo.npc.gz')
-            print(e)
-            raw_input('Showing graph plot, press ENTER to continue')
-            exit()
+    def test_santoro2007(self):
+        N_ITERS = 5
+        N_NETWORKS = 15
+        N_NODES_STEP = 5
 
-N_ITERS = 5
-N_NETWORKS = 15
-N_NODES_STEP = 5
+        node_range = 100
+        nets = [
+            [(100, 100)],
+            [(100, 100), (175, 250), (250, 175), (100, 250),
+                (175, 175), (100, 175)],
+            [(100, 100), (150, 200), (175, 175), (175, 100),
+                (250, 175), (250, 250), (325, 250), (325, 325), (325, 400)]
+        ]
 
-print('Testing special cases')
+        for i, node_positions in enumerate(nets, start=1):
+            net = Network()
+            for node_pos, name in node_positions:
+                net.add_node(pos=node_pos, commRange=node_range)
 
-node_range = 100
-nets = [
-    [(100, 100)],
-    [(100, 100), (175, 250), (250, 175), (100, 250), (175, 175), (100, 175)],
-    [(100, 100), (150, 200), (175, 175), (175, 100), (250, 175), (250, 250),
-        (325, 250), (325, 325), (325, 400)]
-]
+            self.do_test(net, 'Special %d' % i)
 
-for i, node_positions in enumerate(nets, start=1):
-    net = Network()
-    for node_pos in node_positions:
-        net.add_node(pos=node_pos, commRange=node_range)
+        for i in range(N_ITERS):
+            for n_nodes in range(N_NODES_STEP,
+                                 N_NETWORKS*N_NODES_STEP+N_NODES_STEP,
+                                 N_NODES_STEP):
 
-    do_test(net)
-    print('Special case network %d PASS' % i)
-
-print('Testing random networks')
-
-for i in range(N_ITERS):
-    for n_nodes in range(N_NODES_STEP,
-                         N_NETWORKS*N_NODES_STEP+N_NODES_STEP,
-                         N_NODES_STEP):
-
-        net_gen = NetworkGenerator(n_nodes)
-        net = net_gen.generate_random_network()
-        do_test(net)
-        print('Random network with %d nodes PASS' % n_nodes)
+                net_gen = NetworkGenerator(n_nodes)
+                net = net_gen.generate_random_network()
+                self.do_test(net, 'Random %d, %d nodes' % (i, n_nodes))
