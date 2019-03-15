081d2239a97ccfc5f74aca5ab8c516cedbb3da8a
Damir Arbula <damir.arbula@gmail.com>
2019-03-15 16:20:04 +0100

    pep8

--- pymote/algorithms/broadcast.py
+++ pymote/algorithms/broadcast.py
@@ -16,14 +16,18 @@ class Flood(NodeAlgorithm):
                 node.status = 'INITIATOR'
                 ini_nodes.append(node)
         for ini_node in ini_nodes:
-            self.network.outbox.insert(0, Message(header=NodeAlgorithm.INI,
-                                                 destination=ini_node))
+            self.network.outbox.insert(0, Message(
+                header=NodeAlgorithm.INI,
+                destination=ini_node
+            ))
 
     def initiator(self, node, message):
         if message.header == NodeAlgorithm.INI:
             # default destination: send to every neighbor
-            node.send(Message(header='Information',
-                              data=node.memory[self.informationKey]))
+            node.send(Message(
+                header='Information',
+                data=node.memory[self.informationKey]
+            ))
             node.status = 'DONE'
 
     def idle(self, node, message):
@@ -32,17 +36,19 @@ class Flood(NodeAlgorithm):
             destination_nodes = list(node.memory[self.neighborsKey])
             # send to every neighbor-sender
             destination_nodes.remove(message.source)
-            if destination_nodes:
-                node.send(Message(destination=destination_nodes,
-                                  header='Information',
-                                  data=message.data))
+            if len(destination_nodes) > 0:
+                node.send(Message(
+                    destination=destination_nodes,
+                    header='Information',
+                    data=message.data
+                ))
         node.status = 'DONE'
 
     def done(self, node, message):
         pass
 
     STATUS = {
-              'INITIATOR': initiator,
-              'IDLE': idle,
-              'DONE': done,
-             }
+        'INITIATOR': initiator,
+        'IDLE': idle,
+        'DONE': done,
+    }
